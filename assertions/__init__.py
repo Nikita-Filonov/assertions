f94b54f9350240b9dff9b76326816e6e7d0d7f91
Nikita Filonov <filonov.nikitkaa@gmail.com>
2022-03-20 16:14:25 +0300

    Update __init__.py

---
+++ assertions/__init__.py
@@ -0,0 +1,16 @@
+from assertions.assertions import assert_
+from assertions.solutions.common import assert_all, assert_any, assert_lte, assert_truth
+from assertions.solutions.models import assert_model_equal
+from assertions.solutions.response import assert_attr, assert_json, assert_response_status
+
+__all__ = [
+    'assert_',
+    'assert_all',
+    'assert_any',
+    'assert_lte',
+    'assert_truth',
+    'assert_attr',
+    'assert_json',
+    'assert_response_status',
+    'assert_model_equal'
+]
